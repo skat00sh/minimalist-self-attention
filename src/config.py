cd23cb2cb7b3175e59fb41887a14d953af352d30
skatoosh <devendra.y12@lnmiit.ac.in>
2023-03-14 17:08:00 +0100

    Remove extra files

--- src/config.py
+++
@@ -1,7 +0,0 @@
-
-basic_config = {
-    "batch_size" : 64,
-    "learning_rate" : 0.01,
-    
-
-}
--- src/utils.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-
-def to_device():
-    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
